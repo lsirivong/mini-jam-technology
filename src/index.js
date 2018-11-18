04d94fee3b7b95cb69458b1cfe0587ad85f37ee1
Lenny Sirivong <lenny@lenny-s.com>
2018-11-18 11:28:33 -0700

    refactor scene functions into GameScene

--- src/index.js
+++ src/index.js
@@ -20,154 +20,108 @@ let state = {
   map: null
 }
 
-
 const GRID_WIDTH = 20
 const GRID_HEIGHT = 16
 const CELL_SIZE = 16
 
-const EMPTY_HEX = '#ffffff'
-const WALL_HEX  = '#000000'
-const SPOT_HEX = '#00ff00'
-const PLAYER_HEX = '#ff0000'
-const EXIT_HEX = '#0000ff'
-
-var EMPTY = 0;
-var WALL = 1;
-var SPOT = 2;
-var EXIT = 3;
-var PLAYER = 4;
-
-var level = [];
-
-function levelColorToObjectIndex(c) {
-  const hex = Phaser.Display.Color.RGBToString(c.red, c.green, c.blue, c.alpha)
-  switch (hex) {
-    case EMPTY_HEX:
-      return 0
-
-    case WALL_HEX:
-      return WALL
-
-    case SPOT_HEX:
-      return SPOT
-
-    case EXIT_HEX:
-      return EXIT
-
-    case PLAYER_HEX:
-      return PLAYER
+class GamePlayScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'GamePlay'})
   }
 
-  return null
-}
+  preload() {
+    this.load.image('tileset', tilesetUrl)
+    // this.load.image('player_spritesheet', playerUrl)
+    this.load.tilemapTiledJSON({
+      key: 'level1',
+      url: tiledJson
+    });
 
-function loadLevelFromTexture(textures, key) {
-  const level = []
+    _.each(state.actors, actor => {
+      actor.preload.call(actor, this)
+    })
+    state.player.preload.call(state.player, this)
 
-  for (let i = 0; i < GRID_WIDTH; i++) {
-    for (let j = 0; j < GRID_HEIGHT; j++) {
-      if (!level[i]) {
-        level[i] = []
-      }
-
-      const c = textures.getPixel(i, j, key);
-      level[i][j] = levelColorToObjectIndex(c)
-    }
+    this.input.gamepad.once('connected', function (pad) {
+      pad.threshold = 0.5
+    });
   }
 
-  return level
-}
+  loadLevel(key) {
+    state.map = this.make.tilemap({ key: key })
+    const { map } = state
 
-function preload() {
-  this.load.image('tileset', tilesetUrl)
-  // this.load.image('player_spritesheet', playerUrl)
-  this.load.tilemapTiledJSON({
-    key: 'level1',
-    url: tiledJson
-  });
+    var tiles = map.addTilesetImage('tileset');
+    // var playerTiles = map.addTilesetImage('player_spritesheet');
 
-  _.each(state.actors, actor => {
-    actor.preload.call(actor, this)
-  })
-  state.player.preload.call(state.player, this)
+    map.createStaticLayer('background', tiles, 0, 0);
+    map.createStaticLayer('level', tiles, 0, 0);
+    map.createDynamicLayer('cables', tiles, 0, 0);
+    map.createDynamicLayer('items', tiles, 0, 0);
+    const playerLayer = map.createDynamicLayer('player', tiles, 0, 0);
 
-  this.input.gamepad.once('connected', function (pad) {
-    pad.threshold = 0.5
-  });
-}
+    map.setLayer(playerLayer)
 
-function create() {
-  state.map = this.make.tilemap({ key: 'level1' })
-  const { map } = state
+    var playerTile = map.findTile(
+      // find the player tile
+      (tile, i) => {
+        if (tile.index < 0) {
+          return false
+        }
 
-  var tiles = map.addTilesetImage('tileset');
-  // var playerTiles = map.addTilesetImage('player_spritesheet');
+        const firstgid = _.get(tile, 'tileset[0].firstgid')
 
-  map.createStaticLayer('background', tiles, 0, 0);
-  map.createStaticLayer('level', tiles, 0, 0);
-  state.cableLayer = map.createDynamicLayer('cables', tiles, 0, 0);
-  map.createDynamicLayer('items', tiles, 0, 0);
-  state.playerLayer = map.createDynamicLayer('player', tiles, 0, 0);
-
-  map.setLayer(state.playerLayer)
-
-  var playerTile = map.findTile(
-    // find the player tile
-    (tile, i) => {
-      if (tile.index < 0) {
-        return false
+        return (
+          tile.index === (22 + firstgid)
+        )
       }
+    )
 
-      const firstgid = _.get(tile, 'tileset[0].firstgid')
+    map.removeTileAt(playerTile.x, playerTile.y)
 
-      return (
-        tile.index === (22 + firstgid)
-      )
-    }
-  )
+    return [playerTile.x, playerTile.y]
+  }
 
-  _.each(state.actors, actor => {
-    actor.create.call(actor, this)
-  })
+  create() {
+    const [x, y] = this.loadLevel('level1')
 
-  state.player.create.call(state.player, this, playerTile.x, playerTile.y)
-  state.player.emitter.on('move', handlePlayerMove, this)
-  map.removeTileAt(playerTile.x, playerTile.y)
-}
+    _.each(state.actors, actor => {
+      actor.create.call(actor, this)
+    })
 
-function handlePlayerMove(x, y, deltaX, deltaY) {
-  const { map } = state
+    state.player.create.call(state.player, this, x, y)
+    state.player.emitter.on('move', this.handlePlayerMove, this)
+  }
 
-  // put a cable where we used to be
-  // const prevItemTile = map.getTileAt(x - deltaX, y - deltaY, false, 'items')
-  // if (!prevItemTile) {
+  handlePlayerMove(x, y, deltaX, deltaY) {
+    const { map } = state
+
+    // put a cable where we used to be
     const cableIndex = pickCable(deltaX, deltaY, state.player.moveHistory)
     map.putTileAt(cableIndex, x - deltaX, y - deltaY, true, 'cables')
-  // }
 
-  // check for items in the new tile
-  const itemTile = map.getTileAt(x, y, false, 'items')
-  if (_.get(itemTile, 'properties.off_switch')) {
-    // if there's a switch on new position, flip it on
-    const SPR = {
-      ON_SWITCH: 31
-    }
-    map.putTileAt(SPR.ON_SWITCH, x, y, true, 'items')
-  } else if (_.get(itemTile, 'properties.exit')) {
-    // if we're on the exit, check win condition
-    console.log('WIN')
-  }
-
-  // if (!_.get(itemTile, 'properties.prevent_cable')){
-    // unless tile prevents it, draw a cable
+    // draw cable attached to the player
     const underCableIndex = pickCable(-deltaX, -deltaY, null)
     map.putTileAt(underCableIndex, x, y, true, 'cables')
-  // }
-}
 
-function update()
-{
-  updateFn(state, this)
+    // check for items in the new tile
+    const itemTile = map.getTileAt(x, y, false, 'items')
+    if (_.get(itemTile, 'properties.off_switch')) {
+      // if there's a switch on new position, flip it on
+      const SPR = {
+        ON_SWITCH: 31
+      }
+      map.putTileAt(SPR.ON_SWITCH, x, y, true, 'items')
+    } else if (_.get(itemTile, 'properties.exit')) {
+      // if we're on the exit, check win condition
+      console.log('WIN')
+    }
+  }
+
+  update()
+  {
+    updateFn(state, this)
+  }
 }
 
 var config = {
@@ -176,13 +130,7 @@ var config = {
   height: 256,
   canvas: document.querySelector('canvas'),
   pixelArt: true,
-  scene:[LoadScene,MenuScene],
-  /*
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  },*/
+  scene:[LoadScene,MenuScene,GamePlayScene],
   input: {
     gamepad: true
   },
