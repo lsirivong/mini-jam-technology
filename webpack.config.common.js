5301eabf4987130322075ba4a54ef4273be2eaa1
Lenny Sirivong <lenny@lenny-s.com>
2018-11-18 18:39:15 -0700

    Build config

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,23 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = env => ({
-  mode: env && env.production ? 'production' : 'development',
+module.exports = {
   entry: {
     main: './src/index.js',
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Mgmt',
       template: 'src/index.html',
     }),
-    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
@@ -44,4 +38,4 @@ module.exports = env => ({
       }
     ]
   }
-})
+}
