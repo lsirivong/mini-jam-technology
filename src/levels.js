8e7a540e82c076aaab67c41b0a95d9037e4239c7
Lenny Sirivong <lenny@lenny-s.com>
2018-11-18 20:38:27 -0700

    Add level 6

---
+++ src/levels.js
@@ -0,0 +1,37 @@
+import level1Json from './tilemaps/level-1.json'
+import level2Json from './tilemaps/level-2.json'
+import level3Json from './tilemaps/level-3.json'
+import level4Json from './tilemaps/level-4.json'
+import level5Json from './tilemaps/level-5.json'
+import level6Json from './tilemaps/level-6.json'
+
+const levels = [
+  /*
+  {
+    key: 'level1',
+    url: level1Json,
+  },
+  {
+    key: 'level2',
+    url: level2Json
+  },
+  {
+    key: 'level3',
+    url: level3Json
+  },
+  {
+    key: 'level4',
+    url: level4Json
+  },
+  {
+    key: 'level5',
+    url: level5Json
+  },
+  */
+  {
+    key: 'level6',
+    url: level6Json
+  },
+]
+
+export default levels
